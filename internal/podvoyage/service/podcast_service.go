3a348f3d0fda78a0b3a107f3b6182c6c988a44bf
Airel Camilo Khairan <airel.camilo@ui.ac.id>
2023-10-23 11:13:12 +0700

    Add update saved podcast functionality

--- internal/podvoyage/service/podcast_service.go
+++ internal/podvoyage/service/podcast_service.go
@@ -8,17 +8,20 @@ import (
 	"net/http"
 	"net/url"
 	"strconv"
+	"sync"
+	"time"
 
 	"github.com/airelcamilo/podvoyage-backend/internal/podvoyage/model"
 	"gorm.io/gorm"
 )
 
 type PodcastService struct {
-	DB *gorm.DB
+	DB                  *gorm.DB
+	MAX_CONCURRENT_JOBS int
 }
 
 func (s *PodcastService) New(db *gorm.DB) PodcastService {
-	return PodcastService{db}
+	return PodcastService{db, 1000}
 }
 
 func (s *PodcastService) SearchPodcasts(request *model.SearchRequest) (model.SearchResult, error) {
@@ -74,25 +77,102 @@ func (s *PodcastService) SearchPodcast(id int, userId int) (model.Podcast, error
 		return podcast, err
 	}
 
-	for _, episode := range podcast.Episodes {
-		episode.UserId = userId
-		episode.TrackId = podcast.TrackId
+	wg := &sync.WaitGroup{}
+	semaphore := make(chan struct{}, s.MAX_CONCURRENT_JOBS)
+	for i := range podcast.Episodes {
+		semaphore <- struct{}{}
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+
+			podcast.Episodes[i].UserId = userId
+			podcast.Episodes[i].TrackId = podcast.TrackId
+			<-semaphore
+		}(i)
 	}
+	wg.Wait()
 	return podcast, nil
 }
 
 func (s *PodcastService) GetAllPodcast(userId int) ([]model.Podcast, error) {
 	var podcasts []model.Podcast
-	if result := s.DB.Where("user_id = ?", userId).Find(&podcasts); result.Error != nil {
+	if result := s.DB.Debug().Preload("Episodes", func(db *gorm.DB) *gorm.DB {
+		return s.DB.Joins(`
+		JOIN (
+			SELECT MAX(date) AS max_date, podcast_id 
+			FROM episodes WHERE user_id = ? 
+			GROUP BY podcast_id
+		) e1 ON episodes.podcast_id = e1.podcast_id AND date = e1.max_date`, userId)
+	}).Where("user_id = ?", userId).Find(&podcasts); result.Error != nil {
 		return podcasts, result.Error
 	}
+
+	wg := &sync.WaitGroup{}
+	semaphore := make(chan struct{}, s.MAX_CONCURRENT_JOBS)
+
+	for i := range podcasts {
+		lastEpisodeDate, err := time.Parse("2006-01-02 15:04:05 -0700 -0700", string(podcasts[i].Episodes[0].Date))
+		if err != nil {
+			return podcasts, err
+		}
+		if time.Until(lastEpisodeDate).Abs().Hours()/24 < 7 {
+			continue
+		}
+
+		semaphore <- struct{}{}
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+
+			if err := s.updatePodcast(userId, podcasts[i], wg); err != nil {
+				<-semaphore
+				return
+			}
+			<-semaphore
+		}(i)
+	}
+	wg.Wait()
 	return podcasts, nil
+}
+
+func (s *PodcastService) updatePodcast(userId int, oldPodcast model.Podcast, wg *sync.WaitGroup) error {
+	var newPodcast model.Podcast
+	var episodes []model.Episode
+	response, err := http.Get(oldPodcast.FeedUrl)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	dec := xml.NewDecoder(response.Body)
+	err = dec.Decode(&newPodcast)
+	if err != nil {
+		return err
+	}
+
+	for i := range newPodcast.Episodes {
+		if newPodcast.Episodes[i].Title == oldPodcast.Episodes[0].Title {
+			if len(episodes) == 0 {
+				break
+			}
+			if result := s.DB.Create(&episodes); result.Error != nil {
+				return result.Error
+			}
+			break
+		}
+
+		newPodcast.Episodes[i].PodcastId = oldPodcast.Id
+		newPodcast.Episodes[i].TrackId = oldPodcast.TrackId
+		newPodcast.Episodes[i].UserId = userId
+		episodes = append(episodes, *newPodcast.Episodes[i])
+	}
+	return nil
 }
 
 func (s *PodcastService) GetPodcast(id int, userId int) (model.Podcast, error) {
 	var podcast model.Podcast
 	if result := s.DB.Preload("Categories").Preload("Episodes", func(db *gorm.DB) *gorm.DB {
-		return s.DB.Order("episodes.id ASC")
+		return s.DB.Order("episodes.date DESC")
 	}).Where("user_id = ?", userId).First(&podcast, id); result.Error != nil {
 		return podcast, result.Error
 	}
